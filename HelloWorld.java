05a36ca512a4aea80381a80f84a349c5d661568b
Abdulelah Albalawi <alblwiabdulelah@gmail.com>
2026-09-22 09:36:51 +0300

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
-        System.out.println("Greetings from the feature branch!");
+        System.out.println("hi, Abdulelah");
     }
-}
+}                                                       
